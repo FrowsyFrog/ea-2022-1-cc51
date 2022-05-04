ef5d761030f4e9b8567312abe000581b528bc1f8
Elyeet9 <goyasleonardo@gmail.com>
2022-05-03 21:10:30 -0500

    Transformacion de datos NA
    -Eliminando redundancia para datos "SC" y "Undefined"
    -Quitar Views redundantes
    -Transformacion de variables NA en la columna lead_time, volviendolos su promedio
    -Transformar las demas variables NA en un valor aleatorio de su misma columna

--- code/03-preprocess-dataset.r
+++ code/03-preprocess-dataset.r
@@ -1,26 +1,46 @@
 #Pre-procesamiento 
 
-hotel_data[duplicated(hotel_data),]
+#Cargar solo los datos unicos
 hotel_data_pre <- unique(hotel_data)
 
+#Convertir todos los datos no utilizables a NA
 hotel_data_pre[hotel_data_pre == "NULL"] <- NA
 hotel_data_pre[hotel_data_pre == ""] <- NA
 
-hotel_data_pre$reservation_status_date <- as.Date(hotel_data_pre$reservation_status_date,"%m/%d/%Y")
+#Eliminando redundancia (Undefined es lo mismo que SC segun documentacion)
+hotel_data_pre[hotel_data_pre == "Undefined"] <- "SC"
 
-View(hotel_data_pre)
+#Convertir datos de tiempo a Date
+hotel_data_pre$reservation_status_date <- as.Date(hotel_data_pre$reservation_status_date,"%m/%d/%Y")
 
 #Eliminando la columna company porque mas de el 90% de los datos tenian valores inutilizables
 #Ademas, esta columna no afecta a nuestros casos de analisis
 hotel_data_pre2 <- subset(hotel_data_pre,select = -company)
 
-View(hotel_data_pre2)
 #Eliminando filas con valores inutilizables en la columna country(0,41% de los datos)
-hotel_data_pre3 <- hotel_data_pre2[!is.na(hotel_data_pre2$country),]
+hotel_data_pre2 <- hotel_data_pre2[!is.na(hotel_data_pre2$country),]
 
-
-View(hotel_data_pre3)
-
+#Convertir datos inutilizables de lead_time al promedio de los normales
+hotel_data_pre2$lead_time <- ifelse(is.na(hotel_data_pre2$lead_time), mean(hotel_data_pre2$lead_time, na.rm = TRUE), hotel_data_pre2$lead_time)
+#Funciones para cambiar valores NA a valores aleatorios que si existen
+rand.valor <- function(x) {
+  faltantes <- is.na(x)
+  tot.faltantes <- sum(faltantes)
+  x.obs <- x[!faltantes]
+  valorado <- x
+  valorado[faltantes] <- sample(x.obs, tot.faltantes, replace = TRUE)
+  return (valorado)
+}
+random.df <- function(df) {
+  nombres <- names(df)
+  for (nombre in nombres) {
+    df[nombre] <- rand.valor(df[,nombre])
+  }
+  df
+}
+#Asignando valores aleatorios a los NA del dataframe
+hotel_data_pre2 <- random.df(hotel_data_pre2)
+View(hotel_data_pre2)
 #Tranformando variables a factores
 hotel_data_pre3$hotel <- factor(hotel_data_pre3$hotel)#
 hotel_data_pre3$agent <- factor(hotel_data_pre3$agent)#
